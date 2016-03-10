7b41f6fb4f866ec50b4addc7e9da47950b7ed4e1
awiede <andreas.s.wiede@gmail.com>
2016-03-10 12:08:21 -0500

    initial commit of yoeman angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name awiedeWebApp
+ * @description
+ * # awiedeWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('awiedeWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
